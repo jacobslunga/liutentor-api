c684c17531ac99f2e83da7a8855eadeb296e727d
Jacob Slunga <jacobslunga21@yahoo.se>
2026-01-31 16:56:57 +0100

    add CORS policy for localhost and liutentor.se

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import { cors } from "hono/cors";
 import { supabaseMiddleware } from "~/db/supabase";
 import exams from "~/api/v1/exams.routes";
 import { fail } from "~/utils/response";
 import { rateLimit } from "~/middleware/ratelimit";
 
 const app = new Hono().basePath("/api");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://liutentor.se"],
+  })
+);
 
 app.onError((err, c) => {
   if (err instanceof HTTPException) {
